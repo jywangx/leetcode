9dfc91ca7d6cbfeffa130c35ef614abb06c3c0a7
jywangx <wangjingyuan@mail.nwpu.edu.cn>
2023-09-19 10:56:01 +0800

    每日一题-23-09-19

---
+++ daily-question/2560.打家劫舍-iv.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=2560 lang=cpp
+ *
+ * [2560] 打家劫舍 IV
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int lower = *min_element(nums.begin(), nums.end());
+        int upper = *max_element(nums.begin(), nums.end());
+        while (lower <= upper) {
+            int middle = (lower + upper) / 2;
+            int count = 0;
+            bool visited = false;
+            for (int x : nums) {
+                if (x <= middle && !visited) {
+                    count++;
+                    visited = true;
+                } else {
+                    visited = false;
+                }
+            }
+            if (count >= k) {
+                upper = middle - 1;
+            } else {
+                lower = middle + 1;
+            }
+        }
+        return lower;
+    }
+};
+// @lc code=end
+
