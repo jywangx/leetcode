a1c79938e47a3936307e214d0f80a9ac48d3dbfc
jywangx <wangjingyuan@mail.edu.nwpu.cn>
2023-10-04 22:48:01 +0800

    每日一题-23-10-04

---
+++ daily-question/188.买卖股票的最佳时机-iv.cpp
@@ -0,0 +1,1 @@
+../labuladong/dp/sell_stoke/188.买卖股票的最佳时机-iv.cpp
--- labuladong/dp/sell_stoke/188.买卖股票的最佳时机-iv.cpp
+++ labuladong/dp/sell_stoke/188.买卖股票的最佳时机-iv.cpp
@@ -7,8 +7,51 @@
 // @lc code=start
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& prices) {
+    int maxProfit_k_inf(vector<int>& prices) {
+        int n = prices.size();
+        int dp_i_0 = 0, dp_i_1 = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = max(dp_i_1, temp - prices[i]);
+        }
+        return dp_i_0;
+    }
+    int maxProfit(int max_k, vector<int>& prices) {
+        int n = prices.size();
+        if (n <= 0) {
+            return 0;
+        }
+        if (max_k > n / 2) {
+            // 复用之前交易次数 k 没有限制的情况
+            return maxProfit_k_inf(prices);
+        }
 
+        // base case：
+        // dp[-1][...][0] = dp[...][0][0] = 0
+        // dp[-1][...][1] = dp[...][0][1] = -infinity
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(max_k + 1, vector<int>(2)));
+        // k = 0 时的 base case
+        for (int i = 0; i < n; i++) {
+            dp[i][0][1] = INT_MIN;
+            dp[i][0][0] = 0;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int k = max_k; k >= 1; k--) {
+                if (i - 1 == -1) {
+                    // 处理 i = -1 时的 base case
+                    dp[i][k][0] = 0;
+                    dp[i][k][1] = -prices[i];
+                    continue;
+                }
+                dp[i][k][0] = max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
+                dp[i][k][1] =
+                    max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
+            }
+        }
+        return dp[n - 1][max_k][0];
     }
 };
 // @lc code=end
