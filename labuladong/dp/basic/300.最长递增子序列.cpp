1ad5615d5580451041ddc55cf260303f41045b1f
jywangx <wangjingyuan@mail.nwpu.edu.cn>
2023-10-06 22:41:55 +0800

    temp

--- labuladong/dp/basic/300.最长递增子序列.cpp
+++ labuladong/dp/basic/300.最长递增子序列.cpp
@@ -8,7 +8,27 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-
+        int n = nums.size();
+        vector<int> dp(n+1, INT_MIN);
+        int max_len = 0;
+        for (int &num : nums) {
+            if (num > dp[max_len]) {
+                max_len ++;
+                dp[max_len] = num;
+            } else {
+                int left = 1, right = max_len;
+                while (left <= right) {
+                    int mid = (left + right) / 2;
+                    if (dp[mid] < num) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+                dp[left] = num;
+            }
+        }
+        return max_len;
     }
 };
 // @lc code=end
