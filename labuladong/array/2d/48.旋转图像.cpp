1ed6ad38fa01ebbce3b87396afd33f8f41776d46
jywangx <wangjingyuan@mail.nwpu.edu.cn>
2023-10-20 17:01:04 +0800

    labuladong-数组-二维数组-48&54&59&151

--- labuladong/array/2d/48.旋转图像.cpp
+++ labuladong/array/2d/48.旋转图像.cpp
@@ -8,7 +8,21 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-
+        int n = matrix.size();
+        for (int i = 0; i < n; i ++) {
+            for (int j = i + 1; j < n; j ++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int j = 0; j < n; j ++) {
+            for (int il = 0, ir = n-1; il < ir; il ++, ir --) {
+                int temp = matrix[j][il];
+                matrix[j][il] = matrix[j][ir];
+                matrix[j][ir] = temp;
+            }
+        }
     }
 };
 // @lc code=end
