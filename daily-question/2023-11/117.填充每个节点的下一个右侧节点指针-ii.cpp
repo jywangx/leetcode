bb532f3ba7bdc282be4e6d6979684f05cbaa1fb8
jywangx <wangjingyuan@mail.nwpu.edu.cn>
2023-11-09 16:44:52 +0800

    每日一题-23-11-02

---
+++ daily-question/2023-11/117.填充每个节点的下一个右侧节点指针-ii.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=cpp
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == nullptr) return nullptr;
+        queue<Node *> q;
+        q.push(root);
+        q.push(nullptr);
+        while (!q.empty()) {
+            Node *node = q.front();
+            q.pop();
+            if (node == nullptr) {
+                if (q.empty()) {
+                    break;
+                }
+                q.push(nullptr);
+                continue;
+            }
+            node->next = q.front();
+            if (node->left != nullptr) {
+                q.push(node->left);
+            }
+            if (node->right != nullptr) {
+                q.push(node->right);
+            }
+        }
+        return root;
+    }
+};
+// @lc code=end
+
